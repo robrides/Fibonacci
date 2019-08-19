afa54c4c2bec758eb3e8879bbc99126d678358f1
Rob Lounsbury <rob.lounsbury@gmail.com>
2019-08-19 11:44:49 -0500

    added a horribly inefficient FibMethod2 class

---
+++ src/pro/robrides/FibMethod2.java
@@ -0,0 +1,20 @@
+package pro.robrides;
+
+public class FibMethod2 {
+
+	public static long fibonacci(int n) {
+		System.out.println(n);
+        if (n <= 1) {
+        	return n;
+        } else {
+        	return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int i = 0; i <= n; i++)
+            System.out.println(i + ": " + fibonacci(i));
+    }
+
+}
