e67dca75f42fe38b66bb55dc66ed90b5d4aca49a
Rob Lounsbury <rob.lounsbury@gmail.com>
2019-08-19 11:45:53 -0500

    Formatted code

--- src/pro/robrides/FibMethod2.java
+++ src/pro/robrides/FibMethod2.java
@@ -4,17 +4,21 @@ public class FibMethod2 {
 
 	public static long fibonacci(int n) {
 		System.out.println(n);
-        if (n <= 1) {
-        	return n;
-        } else {
-        	return fibonacci(n-1) + fibonacci(n-2);
-        }
-    }
+		if (n <= 1) {
+			return n;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
 
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        for (int i = 0; i <= n; i++)
-            System.out.println(i + ": " + fibonacci(i));
-    }
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+
+		for (int i = 0; i <= n; i++) {
+
+			System.out.println(i + ": " + fibonacci(i));
+
+		}
+	}
 
 }
